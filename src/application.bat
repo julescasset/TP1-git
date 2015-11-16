587adb4c225f1376603e592ca7662ce56c0ca71f
julescasset <casset.jules@gmail.com>
2015-11-16 15:08:56 +0100

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,6 @@
 ### application.bat - 201x - Casset ###
+version = "2"
+build_hour = "11H00"
 echo "Copyright Casset Corp - 201x"
-echo "Build: 10:00"
-echo "version 1.1"
+echo "Build $ {build_hour}"
+echo "version ${version}"
